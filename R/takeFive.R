630fbc365a06e0f202b284404bec7547bef32ede
Diheng Zhang <dihengz@gmail.com>
2016-01-12 17:06:29 -0500

    Import function done

--- R/takeFive.R
+++ R/takeFive.R
@@ -2,9 +2,7 @@
 #'
 #' \code{sum} Import the basic five dataset files generated from PI.
 #'
-#' This function is used to read the all five original dataset files from PI.
-#' It is equal to the read function but name differently as a special function
-#' only for PI dataset. 
+#' This function is used to read the all five original dataset files from PI. It is equal to the read function but name differently as a special function only for PI dataset. 
 #' @param explicit A string for the path of explicit.txt file or the equivalent.
 #' @param iat A string for the path of iat.txt file or the equivalent.
 #' @param sessions A string for the path of sessions.txt file or the equivalent.
@@ -17,10 +15,8 @@
 #'  \dontrun{
 #'      takeFive('~/explicit.txt','~/iat.txt','~/sessions.txt','~/sessionTasks.txt','~/demographics.txt')
 #'    }
-#' @seealso  \code{\link{~/Requirements}} For files naming consistency and 
-#'  dataset cleaning requirements.
-#' @return If all five files are imported correctly, there will be five objects return as original dataset, and there are:
-#'  explicit_origin, iat_origin, sessions_origin, tasks_origin, demo_origin.
+#' @seealso  \code{\link{~/Requirements}} For files naming consistency and dataset cleaning requirements.
+#' @return If all five files are imported correctly, there will be five objects return as original dataset, and there are: explicit_origin, iat_origin, sessions_origin, tasks_origin, demo_origin.
 
 takeFive <- function(explicit,iat,sessions,tasks,demo, strAsFtr = FALSE) {
 	Explicit <- read.delim(explicit, stringsAsFactors=strAsFtr)
