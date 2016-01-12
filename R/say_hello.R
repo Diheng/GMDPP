c3835f7f0aa277733b8f468315101367f74d0f41
Diheng Zhang <dihengz@gmail.com>
2016-01-12 13:42:20 -0500

    First round

---
+++ R/say_hello.R
@@ -0,0 +1,14 @@
+#' Say Hello Function
+#'
+#' Relax, this function just want to sing a song for you. If you happen to know what song it is singing, shoot Diheng an email! He will take you out for a coffee.
+#' @param say a dummy variable, has no influence on the program
+#' @keywords hello
+#' @export 
+#' @examples
+#' say_hello()
+
+
+
+say_hello <- function(say = TRUE) {
+	print("Say hello, oh, oh, oh oh oh!...")
+}
