48b81cd58c0463e5c6dfaf2bc8fb97a7c2bab3f5
Diheng Zhang <dihengz@gmail.com>
2016-01-12 15:18:49 -0500

    Little progress

---
+++ R/prepare.R
@@ -0,0 +1,10 @@
+#' prepare Function
+#' 
+#' This is the main function of GMDPP package. It will take all the standard files you will get from PI or MTurk, transform them into formats that required by PI reseseach team(Insert link here). I t will also return necessary variables that include information that you will find handy in later data analysis. 
+#' @param 
+#' @keywords
+#' @export
+#' @examples
+#' prepare(...)
+
+
